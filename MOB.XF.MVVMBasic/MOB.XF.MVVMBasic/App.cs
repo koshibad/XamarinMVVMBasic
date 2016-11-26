cc82f7dec9afc7d69e917166a2cafe4cf246e5aa
Diogo Koshiba <diogo.koshiba@gmail.com>
2016-11-26 10:04:22 -0800

    creating project

--- MOB.XF.MVVMBasic/MOB.XF.MVVMBasic/App.cs
+++ MOB.XF.MVVMBasic/MOB.XF.MVVMBasic/App.cs
@@ -12,19 +12,7 @@ namespace MOB.XF.MVVMBasic
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new NavigationPage(new View.AlunoViewPage());
         }
 
         protected override void OnStart()
---
+++ MOB.XF.MVVMBasic/MOB.XF.MVVMBasic/View/AlunoViewPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MOB.XF.MVVMBasic.ModelView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MOB.XF.MVVMBasic.View
+{
+    public partial class AlunoViewPage : ContentPage
+    {
+        AlunoViewModel vmAluno;
+
+        public AlunoViewPage()
+        {
+            var aluno = AlunoViewModel.GetAluno();
+            vmAluno = new AlunoViewModel(aluno);
+            BindingContext = vmAluno;
+        }
+    }
+}
